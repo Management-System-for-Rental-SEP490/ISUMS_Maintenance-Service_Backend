db86fba8a5a3585a9a44c3e083174ec8cb43c668
minhduy <MDuy10x203@gmail.com>
2026-03-16 15:04:30 +0700

    modify link to connect swagger

--- src/main/java/com/isums/maintainservice/controllers/PlanHouseController.java
+++ src/main/java/com/isums/maintainservice/controllers/PlanHouseController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/maintenance/planhouses")
+@RequestMapping("/api/maintenance/planhouses")
 @RequiredArgsConstructor
 public class PlanHouseController {
     private final PeriodicInspectionPlanService periodicInspectionPlanService;
